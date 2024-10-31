c72d404ac9d2d7b899c24ed37ca9f7ac3ee062ec
dannielgloria <danniel.gloria@gmail.com>
2024-10-31 17:36:26 -0600

    update App.tsx and LoginForm.tsx

--- src/components/Login/LoginForm.tsx
+++ src/components/Login/LoginForm.tsx
@@ -1,25 +1,35 @@
-import React, { useState } from 'react';
-import { TextField, Button } from '@mui/material';
-import { useReduxDispatch, useReduxSelector } from '../../hooks/reduxHooks'; // Asegúrate de que las rutas sean correctas
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';  // Importa useNavigate
+import { useReduxDispatch, useReduxSelector } from '../../hooks/reduxHooks';
 import { login } from '../../redux/authSlice';
+import './LoginForm.scss'
+import { Button, TextField } from '@mui/material';
 
 export const LoginForm: React.FC = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const dispatch = useReduxDispatch();
+  const isAuthenticated = useReduxSelector((state) => state.auth.isAuthenticated); // Asegúrate de que esto es correcto
   const error = useReduxSelector((state) => state.auth.error);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate('/home');  // Redirecciona cuando el usuario esté autenticado
+    }
+  }, [isAuthenticated, navigate]);  // Dependencias del useEffect
 
   const handleLogin = () => {
     dispatch(login({ username, password }));
   };
 
   return (
-    <div>
-      <h2>Login Form</h2>
+    <div className="currency-converter">
       <TextField
         label="Username"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
+        className="select-field"
         fullWidth
       />
       <TextField
@@ -27,10 +37,13 @@ export const LoginForm: React.FC = () => {
         type="password"
         value={password}
         onChange={(e) => setPassword(e.target.value)}
+        className="select-field"
         fullWidth
       />
-      <Button onClick={handleLogin} variant="contained" color="primary">Entrar</Button>
-      {error && <p>Error: {error}</p>}
+      <div className="button-container">
+            <Button onClick={handleLogin} variant="contained" color="primary">Entrar</Button>
+      </div>
+      {error && <p>Username o Password incorrectos, intenta de nuevo</p>}
     </div>
   );
 };
