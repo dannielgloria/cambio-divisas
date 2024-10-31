30d0b9894d72e867ba517c309b5fa4a167dbddb4
dannielgloria <danniel.gloria@gmail.com>
2024-10-31 17:26:00 -0600

    update src/index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import './index.scss';  // Asegúrate de tener este archivo para estilos globales
+import { Provider } from 'react-redux';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { store } from './redux/store';
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-reportWebVitals();
