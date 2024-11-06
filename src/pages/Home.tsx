08d3e9a398b1db5ba1c6e5df7845f73ab1832dbf
dannielgloria <danniel.gloria@gmail.com>
2024-11-05 19:45:01 -0600

    Feat: se agrega contenedor con una imagen en movimiento

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import CurrencyConverter from "../components/CurrencyConverter/CurrencyConverter";
+import RotatingImage from "../components/Coin/RotatingImage";
+import coin from "../resources/img/coin.png"
 
 const Home: React.FC = () => {
     return (
         <div>
+            <RotatingImage src={coin}/>
             <h1>Cambio de Divisas</h1>
             <CurrencyConverter />
         </div>
